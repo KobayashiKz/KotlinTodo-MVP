2373bc3bbbc8364181b5c9ffa7bc9bcb96057619
KobayashiKz <anuuihsa9711212@gmail.com>
2018-10-19 02:00:19 +0900

    起動画面のレイアウト作成
    ・ドロワーナビゲーション作成
    ・ActionBar作成

---
+++ app/src/main/java/com/kk/todomvpkotlin/todo_mvp_kotlin/tasks/TasksActivity.kt
@@ -0,0 +1,77 @@
+package com.kk.todomvpkotlin.todo_mvp_kotlin.tasks
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import com.kk.todomvpkotlin.todo_mvp_kotlin.R
+
+class TasksActivity : AppCompatActivity() {
+
+    private var mDrawerLayout: DrawerLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_act)
+
+        //Toolbarの設定.サポートライブラリv7を使用
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        // ハンバーガー画像を設定.
+        actionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu)
+        // タップイベントの有効化
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        // ナビゲーションドロワーの設定
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        mDrawerLayout!!.setStatusBarBackground(R.color.colorPrimaryDark)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        setUpDrawerContent(navigationView)
+
+        // TODO FragmentとPresenterを作る
+
+    }
+
+    /**
+     * ナビゲーションドロワーのアイテムがタップされた時の処理
+     */
+    private fun setUpDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.list_navigation_menu_item -> {
+                    // メニューボタンタップ時は特になにもしない
+                }
+                R.id.statistics_navigation_menu_item -> {
+                    // TODO 結果表示画面に遷移させる
+                }
+                else -> {
+                    // アイテム以外の部分タップした際には特になにもしない
+                    // ドロワーナビゲーションが閉じる判定などはフレームワーク側で行われる
+                }
+            }
+
+            menuItem.isChecked = true
+            // ドロワーナビゲーションを閉じる
+            mDrawerLayout?.closeDrawers()
+            true
+        }
+    }
+
+    /**
+     * オプションボタンが押された際の処理
+     */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.getItemId()) {
+            android.R.id.home -> {
+                // ドロワーナビゲーションを表示
+                mDrawerLayout?.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
